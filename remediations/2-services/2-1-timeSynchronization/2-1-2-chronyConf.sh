fb5044b783279d636050ea76c2d1b2aab48268ff
root <tyler.elkink@gmail.com>
2023-06-03 15:43:20 +0000

    Adding new content from intial work on Centos 8 repo

---
+++ remediations/2-services/2-1-timeSynchronization/2-1-2-chronyConf.sh
@@ -0,0 +1,3 @@
+echo "Manual remediation required."
+echo "Add or edit server or pool lines to /etc/chrony.conf as appropriate."
+echo "Add or edit the OPTIONS in /etc/sysconfig/chronyd to include '-u chrony'."
